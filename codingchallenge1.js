ec82899f239aa90568683573a6ee06166355da85
bettercallsully <solomonelkelani@gmail.com>
2021-02-16 19:30:44 -0600

    Microverse Trial2, Coding Challenge 1

---
+++ codingchallenge1.js
@@ -0,0 +1,11 @@
+function designerPdfViewer(h, word) {
+    let alphabet = "abcdefghijklmnopqrstuvwxyz" 
+    let heightOfLetters = [];
+    heightOfLetters[0, 26] 
+    for {
+        (let i = 0 i <word.length i++);
+        heightOfLetters.push
+    }
+
+
+}
